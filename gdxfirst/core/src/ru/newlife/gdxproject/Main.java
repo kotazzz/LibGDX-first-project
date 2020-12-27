4885297b70897ba368bd8824106cfb40ee86aead
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-27 22:22:42 +0300

    Cube and camera

--- gdxfirst/core/src/ru/newlife/gdxproject/Main.java
+++ gdxfirst/core/src/ru/newlife/gdxproject/Main.java
@@ -2,12 +2,13 @@ package ru.newlife.gdxproject;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -16,6 +17,9 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
 
 public class Main extends ApplicationAdapter {
     public Environment environment;
@@ -23,31 +27,73 @@ public class Main extends ApplicationAdapter {
     public ModelBatch modelBatch;
     public Model model;
     public ModelInstance instance;
+    public Vector3 position = new Vector3(10f, 10f, 10f);
+    public Vector3 direction = new Vector3(0f,0f,0f);
+    public float speed = 1f;
 	@Override
 	public void create () {
+		
+			
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
         modelBatch = new ModelBatch();
+        
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
         cam.lookAt(0,0,0);
-        cam.near = 1f;
+        cam.near = 0f;
         cam.far = 300f;
         cam.update();
 
         ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 5f, 
+        model = modelBuilder.createBox(10f, 10f, 10f, 
             new Material(ColorAttribute.createDiffuse(Color.GREEN)),
             Usage.Position | Usage.Normal);
         instance = new ModelInstance(model);
 	}
+	public void update() {
+		if(Gdx.input.isKeyPressed(Keys.D)) {
+			position.x+= speed;
+		}
 
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			position.x-= speed;
+		}
+
+		if(Gdx.input.isKeyPressed(Keys.W)) {
+			position.z-= speed;
+		}
+
+		if(Gdx.input.isKeyPressed(Keys.S)) {
+			position.z+= speed;
+		}
+		
+		if(Gdx.input.isKeyPressed(Keys.SPACE)) {
+			position.y += speed;
+		}
+		
+		if(Gdx.input.isKeyPressed(Keys.SHIFT_LEFT)) {
+			position.y-= speed;
+		}
+		
+		if(Gdx.input.isButtonPressed(Buttons.LEFT)) {
+			direction.x = Gdx.input.getDeltaX();
+			direction.y = Gdx.input.getDeltaY();
+		}
+		
+		
+	}
 	@Override
 	public void render () {
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		update();
+		cam.position.set(position);
+		cam.translate(direction);
+		cam.update();
+		
+		System.out.println("dx: "+direction.x+" dy: "+direction.y+" x: "+position.x+" y: "+position.y+" z: "+position.z);
+		
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
+        
         modelBatch.begin(cam);
         modelBatch.render(instance, environment);
         modelBatch.end();
