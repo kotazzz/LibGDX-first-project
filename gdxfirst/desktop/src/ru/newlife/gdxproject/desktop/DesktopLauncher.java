136cf777ba8ef55774dd471d3e3da5040f566d60
NewLife1324 <61694357+NewLife1324@users.noreply.github.com>
2020-12-27 20:21:13 +0300

    create project

---
+++ gdxfirst/desktop/src/ru/newlife/gdxproject/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.newlife.gdxproject.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.newlife.gdxproject.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
